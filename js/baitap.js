5957ab9025b270f2784e9f2a4defe1a3189a8623
Minh <huuminh09dt2@gmail.com>
2023-05-19 01:57:12 +0700

    thêm code

--- js/baitap.js
+++ js/baitap.js
@@ -28,3 +28,27 @@ function demSD(){
     document.getElementById("result2").innerHTML = "Tổng số dương: " + dem;
 }
 document.getElementById("btnDemSoDuong").onclick =demSD;
+
+// Tìm số nhỏ nhất
+function timSoMin(){
+    var soMin = mangSoNguyen[0];
+    for(var i = 1; i < mangSoNguyen.length; i++){
+        if(mangSoNguyen[i] < soMin){
+            soMin = mangSoNguyen[i];
+        }
+    }
+    document.getElementById("result3").innerHTML = "Số nhỏ nhất: " + soMin;
+}
+document.getElementById("btnSoNhoNhat").onclick = timSoMin;
+
+// Tìm số dương nhỏ nhất
+function timSoDuongMin(){
+    var soDuongMin = mangSoNguyen[0];
+    for(var i = 1; i < mangSoNguyen.length; i++){
+        if(mangSoNguyen[i] > 0 && mangSoNguyen[i] < soDuongMin){
+            soDuongMin = mangSoNguyen[i];
+        }
+    }
+    document.getElementById("result4").innerHTML = "Số dương nhỏ nhất: " + soDuongMin;
+}
+document.getElementById("btnSoDuongNhoNhat").onclick = timSoDuongMin;
